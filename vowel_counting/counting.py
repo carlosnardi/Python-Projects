57806d182716ee54e461648c82a590b81b05635c
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2025-11-09 01:29:49 +0000

    Created a vowel counting app

--- vowel_counting/counting.py
+++ vowel_counting/counting.py
@@ -1,11 +1,27 @@
-Mariana é professora de língua portuguesa e quer um programa que conte quantas vogais há em um texto digitado pelos alunos. Isso ajudará a analisar a estrutura das palavras utilizadas.
+# Challenge:
 
-Crie um programa que peça um texto e exiba quantas vogais (a, e, i, o, u) ele contém.
+# Mariana é professora de língua portuguesa e quer um programa que conte quantas vogais há em um texto digitado pelos alunos. Isso ajudará a analisar a estrutura das palavras utilizadas.
 
-Exemplo de entrada:
+# Crie um programa que peça um texto e exiba quantas vogais (a, e, i, o, u) ele contém.
 
-Digite um texto: A linguagem Python é muito versátil.  
-Copiar código
-Saída esperada:
+# Exemplo de entrada:
+# Digite um texto: A linguagem Python é muito versátil.  
 
-O texto contém 13 vogais.  
+# Saída esperada:
+# O texto contém 13 vogais.
+
+def prepare_text(sentence):
+  to_change = 'áéíóúãẽĩõũâêîôû'
+  for char in to_change:
+    if char in sentence:
+      sentence = sentence.replace(char,'a')
+  return sentence.lower()
+
+def check_vowel_number(text):
+  sentence = prepare_text(text)
+  vowels = 'aeiou'
+  num_of_vowels = 0
+  for letter in sentence:
+    if letter in vowels:
+      num_of_vowels += 1
+  return num_of_vowels
---
+++ vowel_counting/main.py
@@ -0,0 +1,5 @@
+from vowel_counting.counting import check_vowel_number
+
+text = input('Enter a text to count the number of vowels: ')
+
+print(f'The sentence has {check_vowel_number(text)} vowels')
