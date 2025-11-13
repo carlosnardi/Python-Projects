91c3de4832ac80f786d2d552b225307b9c3b89ca
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2025-11-13 23:49:49 +0000

    Improvements in find long words app

--- long_words/main.py
+++ long_words/main.py
@@ -14,11 +14,48 @@
 
 text = 'A programação estruturada facilitou o desenvolvimento de grandes sistemas computacionais'
 
-text = text.split(' ')
-extensive_words = []
-for item in text:
-  if len(item) >= 10:
-    extensive_words.append(item)
+# First Option:
+def check_long_words(text):
+  text = text.split(' ')
+  long_words = []
+  for item in text:
+    if len(item) >= 10:
+      long_words.append(item)
+  return long_words
 
-print(extensive_words)
+def print_long_words(text):
+  list_of_words = check_long_words(text)
+  if list_of_words == []:
+    result = 'No long words were found in the text.'
+  else:
+    result = 'Long words found: '
+    for item in list_of_words:
+      if result == 'Long words found: ':
+        result += item  
+      else:
+        result = result + (', ' + item)
+  return result
+  
+print(print_long_words(text))
 
+
+# Second Option:
+# txt = text.split(' ')
+# long_words2 = []
+# for item in txt:
+#   if len(item) >= 10:
+#     long_words2.append(item)
+#   else: 
+#     pass
+
+# result = 'Long words found: '
+# if long_words2 == []:
+#   result = 'No long words were found in the text.'
+# else: 
+#   for item in long_words2:
+#     if result == 'Long words found: ':
+#       result += item  
+#     else:
+#       result = result + (', ' + item)
+
+# print(result)
